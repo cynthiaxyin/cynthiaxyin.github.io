77bf2c9025fe9f35394695b110128aa46a8f174d
Cynthia Yin <cynthiayin@stanford.edu>
2018-02-27 22:31:46 -0800

    CS 270 update for milestone

--- cs270/example/app.js
+++ cs270/example/app.js
@@ -25,7 +25,7 @@ app.controller('Principal',['$scope','$compile',function($scope,$compile){
     afterHeader: '<div class="drop-icon-wrapper sir-accordion-vertical-align"><i class="glyphicon glyphicon-chevron-down"></i></div>',
     topContentClass: '',
     beforeTopContent: '',
-    afterTopContent: '<div><p><small>I repeat through all accordion</small></p></div>',
+    afterTopContent: '<div><p><small></small></p></div>', 
     bottomContentClass: '',
     beforeBottomContent: '',
     afterBottomContent: ''
@@ -33,51 +33,129 @@ app.controller('Principal',['$scope','$compile',function($scope,$compile){
 
   $scope.accordionArray = 
   [
-    {"title":"Level 1","topContent":"This is the top content attr","bottomContent":null,"subCollection":[
-      {"title":"This is a Level 2 Header!","topContent":"This is some nice text right here","bottomContent":null},
-      {"title":"Level 2","topContent":"<pre>And you can ad Html code directly to the content too</pre>","bottomContent":null},
-      {"title":"This Level 2 Header has another subCollection","topContent":null,"bottomContent":"<p>You can choose to put text after the subcollection if you prefer (or both before and after)</p>","subCollection":[
-        {"title":"And You got to the Level 3","topContent":"<span>Awesome text or HTML Content Here</span>"},
-        {"title":"Level 3","topContent":"You can also add AngularJS expressions","subCollection":[
-          {"title":"And another Level, Level 4","topContent":"<p>Awesome text or HTML Content Here</p>"},
-          {"title":"Level 4","topContent":"<p>Adding HTML</p><div style=\"display:inline-block;width:30%;height:100px;background:blue;margin:5px;padding:5px;\"></div><div style=\"display:inline-block;width:30%;height:100px;background:red;margin:5px;padding:5px;\"></div>"}
+    /* Mental Health */
+    {"title":"Mental Health","topContent":"Browse by issue","bottomContent":null,"subCollection":[
+      {"title":"Mental Health Awareness","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
         ]},
-        {"title":"Level 3","topContent":"<p>Awesome text or HTML Content Here</p>"}
-      ]},
-      {"title":"Level 2","topContent":"<p>You can keep adding Levels</p>","bottomContent":null, "subCollection":[
-        {"title":"Level 3","topContent":"<p>You can keep adding Levels</p>","bottomContent":null, "subCollection":[
-          {"title":"Level 4","topContent":"<p>You can keep adding Levels</p>","bottomContent":null}
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Office Residential Education","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Stanford + Mental Health","topContent":null,"bottomContent":null},
+          {"title":"Stanford Mental Health Outreach","topContent":null,"bottomContent":null},
+          {"title":"The Bridge Peer Counseling Center","topContent":null,"bottomContent":null},
+          {"title":"We Continue","topContent":null,"bottomContent":null}
         ]}
-      ]}
-    ]},
-    {"title":"Level 1","topContent":null,"bottomContent":null,"subCollection":[
-      {"title":"Level 2","topContent":"<p>You Can set a Content, a subCollection of items, or combine them; making an element expand to a collection of items with text on the top, bottom or both!.</p>\n<p>Check the next items for examples</p>","bottomContent":null},
-      {"title":"Level 2","topContent":"This is a top content","bottomContent":"This text should be the content after the Items","subCollection":[
-        {"title":"Level 3 item","topContent":"Awesome text"},
-        {"title":"Level 3 item","topContent":"Awesome text"}
       ]},
-      {"title":"Level 2","topContent":"<h3>sir-accordion is also perfect for responsive Design!</h3>\n<strong>The content uses VelocityJS, and you can resize the window and it will adapt pretty well</strong>\n<p>You can try it out by using the content provided or adding your own content and resizing your desktop window, it's awesome</p>","bottomContent":null}
+
+      {"title":"Mental Health Crisis Management","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Office Dean of Students","topContent":null,"bottomContent":null},
+          {"title":"Office Residential Education","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+        ]}
+      ]},
+
+      {"title":"Mental Health Suicide Prevention","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"We Continue","topContent":null,"bottomContent":null}
+        ]}
+      ]},
+
+      {"title":"Mental Health Treatment","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Office Counseling and Psychological Services","topContent":null,"bottomContent":null},
+          {"title":"Office Vaden","topContent":null,"bottomContent":null},
+          {"title":"Office Weiland Health Initiative","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"The Bridge Peer Counseling Center","topContent":null,"bottomContent":null}
+        ]}
+      ]},
+
+      {"title":"Mental Health Wellness","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Office of Religious Life","topContent":null,"bottomContent":null},
+          {"title":"Office Vaden","topContent":null,"bottomContent":null},
+          {"title":"Office Weiland Health Initiative","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"The Bridge Peer Counseling Center","topContent":null,"bottomContent":null},
+          {"title":"The Stanford Happiness Collective","topContent":null,"bottomContent":null}
+        ]}
+      ]},
     ]},
-    {"title":"Level 1","topContent":"This one only goes to Level 1 and the content in it and this is the topContent","bottomContent":"this is the bottom content"},
-    {"title":"Level 1","topContent":"This is the topContent field on this element and it also has an array of items (subCollection array field in the JSON object)","bottomContent":null,"subCollection":[
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null}
-    ]},
-    {"title":"Level 1","topContent":null,"bottomContent":null,"subCollection":[
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null}
-    ]},
-    {"title":"Level 1","topContent":null,"bottomContent":null,"subCollection":[
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null}
-    ]},
-    {"title":"Level 1","topContent":null,"bottomContent":null,"subCollection":[
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null}
+
+    
+    /* Sexual Violence */
+    {"title":"Sexual Violence","topContent":"Browse by issue","bottomContent":null,"subCollection":[
+      {"title":"Sexual Violence Advocacy","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"ASSU Exec","topContent":null,"bottomContent":null},
+          {"title":"Stanford Association of Students for Sexual Assault Prevention","topContent":null,"bottomContent":null},
+        ]}
+      ]},
+
+      {"title":"Sexual Violence Campus Climate Survey","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Campus Climate Survey Committee","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Office of Equity and Access","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+        ]}
+      ]},
+
+      {"title":"Sexual Violence Education and Prevention","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Sexual Assault and Relationship Abuse Advisory Board","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+        ]}
+      ]},
+
+      {"title":"Sexual Violence Survivor Resources","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Confidential Support Team Advisory Board","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Office Confidential Support Team","topContent":null,"bottomContent":null},
+          {"title":"Office of Equity and Access","topContent":null,"bottomContent":null},
+          {"title":"Office Sexual and Relationship Abuse","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+        ]}
+      ]},
+
+      {"title":"Sexual Violence Title IX","topContent":null,"bottomContent":null, "subCollection":[
+        {"title":"Committees","topContent":null,"bottomContent":null, "subCollection":[
+        ]},
+        {"title":"Offices","topContent":null,"bottomContent":null, "subCollection":[
+          {"title":"Office of Equity and Access","topContent":null,"bottomContent":null},
+          {"title":"Office Title IX","topContent":null,"bottomContent":null}
+        ]},
+        {"title":"Student Groups","topContent":null,"bottomContent":null, "subCollection":[
+        ]}
+      ]},
     ]}
   ];
 
